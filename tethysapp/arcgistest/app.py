c2665b4b959ef954b4837d4b924d8b4b94c105d5
jeffderricott <jeffderricott@gmail.com>
2019-03-21 16:43:22 -0400

    Initial commit

---
+++ tethysapp/arcgistest/app.py
@@ -0,0 +1,39 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class Arcgistest(TethysAppBase):
+    """
+    Tethys app class for arcgistest.
+    """
+
+    name = 'arcgistest'
+    index = 'arcgistest:home'
+    icon = 'arcgistest/images/icon.gif'
+    package = 'arcgistest'
+    root_url = 'arcgistest'
+    color = '#8e44ad'
+    description = 'this is my second attempt'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='arcgistest',
+                controller='arcgistest.controllers.home'
+            ),
+            UrlMap(
+                name='service2',
+                url='arcgistest/service2',
+                controller='arcgistest.controllers.service2'
+            ),
+        )
+
+        return url_maps
